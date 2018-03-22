45c77e2d1c561e89e7856e29cc550948e0d6af6a
Leiflee <leiflee@catchplay.com>
2018-03-22 10:22:53 +0800

    1. Friday: fix url match

--- script/background.js
+++ script/background.js
@@ -1,7 +1,7 @@
 chrome.tabs.onUpdated.addListener(function (tabId, change, tab) {
   if (tab.url == undefined) {
     return;
-  } else if (tab.url.match(/http:\/\/video\.friday\.tw\/movie\/detail\/*/) !== null) {
+  } else if (tab.url.match(/https:\/\/video\.friday\.tw\/movie\/detail\/*/) !== null) {
     chrome.browserAction.setIcon({path: 'icon/icon-fr.png', tabId: tabId});
   } else if (tab.url.match(/https:\/\/www\.catchplay\.com/) !== null) {
     chrome.browserAction.setIcon({path: 'icon/icon-cp.png', tabId: tabId});
